d74f83955b7fa24efc5fea8b22bb0b840314ba8d
Naem Haddad <naemhd@gmail.com>
2021-08-09 19:18:48 -0500

    adding pagination to airlines, and fix update api and delete api

--- src/main/java/edu/miu/cs/cs544/ea_ars/controller/AirlineController.java
+++ src/main/java/edu/miu/cs/cs544/ea_ars/controller/AirlineController.java
@@ -3,12 +3,14 @@ package edu.miu.cs.cs544.ea_ars.controller;
 import edu.miu.cs.cs544.ea_ars.domain.Airline;
 import edu.miu.cs.cs544.ea_ars.service.AirlineService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 @RestController
 @RequestMapping("/airlines")
@@ -17,20 +19,15 @@ public class AirlineController {
     @Autowired
     private AirlineService airlineService;
 
-    @GetMapping
-    public List<Airline> findAll(){
-        return airlineService.getAllAirlines();
+    @GetMapping(params = "paged=true")
+    public Page<Airline> findAll(Pageable pageable){
+        return airlineService.getAllAirlines(pageable);
     }
 
     @GetMapping("/{id}")
     public Airline findById(@PathVariable(name = "id") Long airlineId) {
         return airlineService.findById(airlineId);
     }
-
-//    @GetMapping(params = "paged=true")
-//    public Page<Airline> findAll(Pageable pageable) {
-//        return countryRepository.findAll(pageable);
-//    }
 
     @PostMapping
     public Object createAirline(@Valid @RequestBody Airline airline, BindingResult result) {
@@ -42,10 +39,14 @@ public class AirlineController {
     }
 
     @PutMapping("/{id}")
-    public Airline updateAirline(@PathVariable(name = "id") Long airlineId,
-                                 @Valid @RequestBody Airline airline) {
-//        countryRepository.findById(countryId).orElseThrow(RuntimeException::new);
-        return airlineService.save(airline);
+    public Object updateAirline(@PathVariable(name = "id") Long airlineId,
+                                 @Valid @RequestBody Airline airline, BindingResult result) {
+        if (!result.hasErrors()) {
+            airline.setId(airlineId);
+            return airlineService.save(airline);
+        }else{
+            return result.getAllErrors();
+        }
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/edu/miu/cs/cs544/ea_ars/service/AirlineService.java
+++ src/main/java/edu/miu/cs/cs544/ea_ars/service/AirlineService.java
@@ -2,6 +2,8 @@ package edu.miu.cs.cs544.ea_ars.service;
 
 import edu.miu.cs.cs544.ea_ars.domain.Airline;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import edu.miu.cs.cs544.ea_ars.repository.AirlineRepository;
@@ -14,8 +16,8 @@ public class AirlineService {
     @Autowired
     private AirlineRepository airlineRepository;
 
-    public List<Airline> getAllAirlines(){
-        return airlineRepository.findAll();
+    public Page<Airline> getAllAirlines(Pageable pageable){
+        return airlineRepository.findAll(pageable);
     }
 
     public Airline findById(Long airlineId){
