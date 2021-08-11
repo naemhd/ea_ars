8a04201d21e0f5cdfdc2390f2515e9a7526ed28d
Naem Haddad <naemhd@gmail.com>
2021-08-11 11:34:23 -0500

    fix missing import

--- src/main/java/edu/miu/cs/cs544/ea_ars/jms/EmailService.java
+++ src/main/java/edu/miu/cs/cs544/ea_ars/jms/EmailService.java
@@ -3,6 +3,7 @@ package edu.miu.cs.cs544.ea_ars.jms;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
